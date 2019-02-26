eb87610a2aec1a73e571a910472efeea2ea0eaa1
Greg Baker <gregbakerstl@gmail.com>
2019-02-26 13:52:31 -0600

    refactor, comments, forking, naming conventions

--- primality.sh
+++ primality.sh
@@ -3,7 +3,7 @@
 function generator() {
 # number sequence generator
   # high and low range
-  high="10000"
+  high="2000"
   low="1000"
 
   # generate a string of numbers
@@ -14,13 +14,13 @@ function sum_factor_three() {
 # sum integers in string and modulas
 
   # sum
-  total=$(echo $req|sed 's/ /+/g'|bc -l);
+  total=$(echo $candidate|sed 's/ /+/g'|bc -l);
 
   # modulas sum by three cleanly
   if [ $((total%3)) -eq 0 ]; then
     : # noop
   else
-    echo $req # return string
+    echo $candidate # return string
   fi
 }
 
@@ -28,10 +28,10 @@ function factor_seven() {
 # there is not arithmatic shortcut for factoring 7...
 # this is the only function that actually factors the total string!
 
-  if [ $((req%7)) -eq 0 ]; then
+  if [ $((candidate%7)) -eq 0 ]; then
     : # noop
   else
-    echo $req # return string
+    echo $candidate # return string
   fi
 }
 
@@ -39,27 +39,28 @@ function factor_substring() {
 # match the last character of the string
 
   # substring last character
-  last="${req: -1}";
+  last="${candidate:-1}";
 
-  # check if last character matchs an composite number or a number divisable by 5
+  # check if last character matches an composite number or a number divisable by 5
   # NO MATH
-  candidate=$(echo $last|grep -v -E '0|2|4|5|6|8');
+  match=$(echo $last|grep -v -E '0|2|4|5|6|8');
 
-  if [ -z $candidate ]; then
+  if [ -z $match ]; then
     : # noop
   else 
-    echo $req # return string
+    echo $match # return candidate
+    
   fi
 }
 
 function main() {
 
-  while read req; do
-    factor_substring $req  &
-  done| while read req; do
-      sum_factor_three $req &
-    done| while read req; do
-        factor_seven $req; 
+  while read candidate; do
+    factor_substring $candidate  &
+  done | while read candidate; do
+    sum_factor_three $candidate &
+    done | while read candidate; do
+      factor_seven $candidate
       done
 }
 generator|main
