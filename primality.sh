6b75c4efde30ca328b61eecc8c48074ff8b26787
Greg Baker <gregbakerstl@gmail.com>
2019-02-26 12:04:24 -0600

    Update primality.sh

--- primality.sh
+++ primality.sh
@@ -2,97 +2,64 @@
 
 function generator() {
 # number sequence generator
-  # accept an argument for high and low range
-  low="0"
-  high="1000"
+  # high and low range
+  high="10000"
+  low="1000"
 
-  # generate a space dillemited string of numbers
-  seq $low $high|sed 's/ /\n/g'
+  # generate a string of numbers
+  seq $low $high 
 }
 
-function even() {
-# determine if a number is even or odd
-  # 
-  if [ $((req%2)) -eq 0 ]; then
-    : # noop
-  else
-    # return odd numbers back
-    echo $req
-  fi
-}
+function sum_factor_three() {
+# sum integers in string and modulas
 
-function three() {
-# determine if a number is even or odd
-
-  # sum total string
+  # sum
   total=$(echo $req|sed 's/ /+/g'|bc -l);
 
-  # divide sum by 3 cleanly
+  # modulas sum by three cleanly
   if [ $((total%3)) -eq 0 ]; then
     : # noop
   else
-    # return number not divisable by three back
-    echo $req
+    echo $req # return string
   fi
 }
 
-function five() {
-  if [ $((req%5)) -eq 0 ]; then
-    : # noop
-  else
-    # return numbers not divisable by 5 back
-    echo $req
-  fi
-}
+function factor_seven() {
+# there is not arithmatic shortcut for factoring 7...
+# this is the only function that actually factors the total string!
 
-function seven() {
   if [ $((req%7)) -eq 0 ]; then
     : # noop
   else
-    # return numbers not divisable by seven
-    echo $req
+    echo $req # return string
   fi
 }
 
-
-function stringLast() {
+function factor_substring() {
 # match the last character of the string
 
   # substring last character
   last="${req: -1}";
-  # check if last character matchs an even number or a number divisable by 5
+
+  # check if last character matchs an composite number or a number divisable by 5
   # NO MATH
   candidate=$(echo $last|grep -v -E '0|2|4|5|6|8');
 
   if [ -z $candidate ]; then
     : # noop
   else 
-    echo $req
+    echo $req # return string
   fi
-
 }
 
-workflow () {
+function main() {
 
   while read req; do
-    # get the last number of the string
-    # even $last
-    stringLast $req
-  done|
-    #while read req; do
-    #  last="${req: -1}";
-    #  even $last
-    #done|
-      while read req; do
-        three $req
-      done|
-    #    while read req; do
-    #      five $req;
-    #    done|
-         while read req; do
-           seven $req;
-         done
-
+    factor_substring $req  &
+  done| while read req; do
+      sum_factor_three $req &
+    done| while read req; do
+        factor_seven $req; 
+      done
 }
-generator|workflow #|wc -l
-#generator|workflow|wc -l
+generator|main
